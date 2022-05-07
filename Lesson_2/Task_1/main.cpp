c224a75dd772f2bdc3fd65908e044425298796fb
OmelchenkoStas <stas.omelchenko.127@mail.ru>
2022-05-07 14:16:26 +0300

    Task 1 modified

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int num, sum;
     cout << "Enter your number: ";
-    cin >> number;
+    cin >> num;
     for(int i = num; i > 0; i/=10){
         if(i%2 == 0){
             sum +=i%10;
