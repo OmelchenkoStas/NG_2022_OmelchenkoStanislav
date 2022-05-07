54e91b052dbebf6bd83ef5835e2f5bdb14bd0b8f
OmelchenkoStas <stas.omelchenko.127@mail.ru>
2022-05-07 14:14:49 +0300

    Added Lesson 2, Task 2

---
+++ Lesson_2/Task_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num, sum;
+    cout << "Enter your number: ";
+    cin >> number;
+    for(int i = num; i > 0; i/=10){
+        if(i%2 == 0){
+            sum +=i%10;
+        }
+    }
+    cout << "Sum of even numbers: " << sum;
+    return 0;
+}
